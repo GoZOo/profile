29a449c49039b8bbf525efc23f3610c751827318
GoZ <goz@226961.no-reply.drupal.org>
2016-01-18 17:12:31 +0100

    Issue #2652170 by GoZ: Display profiles in a profile options widget

---
+++ src/Plugin/Field/FieldWidget/ProfileOptionsWidget.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\profile\Plugin\Field\FieldWidget\ProfileOptionsWidget.
+ */
+
+namespace Drupal\profile\Plugin\Field\FieldWidget;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsWidgetBase;
+
+/**
+ * Plugin implementation of the 'profile_options' widget.
+ *
+ * @FieldWidget(
+ *   id = "profile_options",
+ *   label = @Translation("Profile Options list"),
+ *   field_types = {
+ *     "entity_reference",
+ *   },
+ *   multiple_values = FALSE
+ * )
+ */
+class ProfileOptionsWidget extends OptionsWidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element = parent::formElement($items, $delta, $element, $form, $form_state);
+
+    $element += array(
+      '#type' => 'radios',
+      '#options' => $this->getOptions($items->getEntity()),
+      '#default_value' => $this->getSelectedOptions($items, $delta),
+      '#multiple' => FALSE,
+      '#attached' => [
+        'library' => [
+          'profile/drupal.profile-options-widget',
+        ],
+      ],
+      '#attributes' => [
+        'class' => [
+          'profile-options-widget'
+        ],
+      ],
+    );
+
+    return $element;
+  }
+
+  /**
+   * Returns the array of options for the widget.
+   *
+   * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
+   *   The entity for which to return options.
+   *
+   * @return array
+   *   The array of options for the widget.
+   */
+  protected function getOptions(FieldableEntityInterface $entity) {
+    if (!isset($this->options)) {
+      $options = [];
+
+      // Limit the settable options for the current user account.
+      $handler_settings = $this->fieldDefinition->getSetting('handler_settings');
+      $view_builder = \Drupal::entityTypeManager()->getViewBuilder('profile');
+      $renderer = \Drupal::service('renderer');
+      foreach ($handler_settings['target_bundles'] as $bundle) {
+        $profiles = \Drupal::entityTypeManager()->getStorage('profile')->loadMultipleByUser(\Drupal::currentUser(), $bundle);
+        foreach ($profiles as $profile) {
+          $renderable = $view_builder->view($profile, 'profile.profile-options-widget');
+          $options[$profile->id()] = $renderer->render($renderable);
+        }
+      }
+
+      // Add an empty option if the widget needs one.
+      if ($empty_label = $this->getEmptyLabel()) {
+        $options = ['_none' => $empty_label] + $options;
+      }
+
+      $module_handler = \Drupal::moduleHandler();
+      $context = array(
+        'fieldDefinition' => $this->fieldDefinition,
+        'entity' => $entity,
+      );
+      $module_handler->alter('options_list', $options, $context);
+
+      array_walk_recursive($options, array($this, 'sanitizeLabel'));
+
+      // Options might be nested ("optgroups"). If the widget does not support
+      // nested options, flatten the list.
+      if (!$this->supportsGroups()) {
+        $options = OptGroup::flattenOptions($options);
+      }
+
+      $this->options = $options;
+    }
+    return $this->options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function sanitizeLabel(&$label) {
+    // Select form inputs allow unencoded HTML entities, but no HTML tags.
+    $label = Html::decodeEntities($label);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function supportsGroups() {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEmptyLabel() {
+    // Add a 'none' option for non-required fields, and a 'select a value'
+    // option for required fields that do not come with a value selected.
+    if (!$this->required) {
+      return t('- None -');
+    }
+  }
+
+}
--- src/ProfileViewBuilder.php
+++ src/ProfileViewBuilder.php
@@ -9,6 +9,7 @@ namespace Drupal\profile;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityViewBuilder;
+use Drupal\Core\Render\Element;
 
 /**
  * Render controller for profile entities.
@@ -22,6 +23,25 @@ class ProfileViewBuilder extends EntityViewBuilder {
     $defaults = parent::getBuildDefaults($entity, $view_mode);
     $defaults['#theme'] = 'profile';
     return $defaults;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildComponents(array &$build, array $entities, array $displays, $view_mode) {
+    parent::buildComponents($build, $entities, $displays, $view_mode);
+
+    // Fake view mode user by profile-options-widget to hide labels.
+    if ($view_mode == 'profile.profile-options-widget') {
+      // Remove labels from display
+      foreach ($build as &$display_build) {
+        foreach (Element::children($display_build) as $field_name) {
+          if (isset($display_build[$field_name]['#label_display'])) {
+            $display_build[$field_name]['#label_display'] = 'hidden';
+          }
+        }
+      }
+    }
   }
 
 }
